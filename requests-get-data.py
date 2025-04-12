a3587cc4461259fb30074f534a33cef8e15bec3b
Berkant Gunes <gunesberkant96@gmail.com>
2025-04-12 13:42:19 +0300

    Update requests-get-data.py

--- requests-get-data.py
+++ requests-get-data.py
@@ -1,6 +1,6 @@
 import requests
 
-username = "apify" # github username
+username = "YOUR GITHUB USERNAME"
 url = f"https://api.github.com/users/{username}"
 user_data = requests.get(url)
 print(user_data.json())
